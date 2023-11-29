617499b8c191d3e73ccfe89be09a3848cb5d66a0
linhua <linhua@copyfun.one>
2023-11-29 17:18:49 +0800

    fix pom

---
+++ src/main/java/io/github/sso/Application.java
@@ -0,0 +1,24 @@
+package io.github.sso;/**
+ * @author Application
+ * @date 2023/11/29
+ */
+
+/**
+ * <p>Title: Application</p>
+ * <p>Description: Application</p>
+ * <p>Copyright: Copyright (c) 2023</p>
+ * <p>Company: TD Co., Ltd.</p>
+ * <p>Create Time: 2023/11/29</p>
+ *
+ * @author TD00857
+ * <p>Update Time: 16:44</p>
+ * <p>Updater: </p>
+ * <p>Update Comments: </p>
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
